8b9ac6c080b1b51f8bac43c633304eb252efd1fa
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-04-04 15:53:01 +0200

    Fixed view update from scope

--- www/js/controllers/main_ctrl.js
+++ www/js/controllers/main_ctrl.js
@@ -31,18 +31,9 @@ function main_ctrl($scope, $ionicPlatform, $timeout, $ionicModal, $ionicListDele
             // Execute action
         });
 
-        $scope.$on('$ionicView.loaded', function(){
-            // Anything you can think of
-            console.log('main controlelr loaded');
-        });
-
-
-
         $scope.$on('$ionicView.enter', function(){
             // Anything you can think of
             $scope.player = MPDService.getPlayer();
-            console.log('enter event');
-            console.log($scope.player);
         });
 
         $scope.seek = function (dummycounter) {
@@ -51,32 +42,10 @@ function main_ctrl($scope, $ionicPlatform, $timeout, $ionicModal, $ionicListDele
             MPDService.seek(newTime);
         };
 
-
-        /**
-         * Cette événement est appellé depuis le service quand qq condition a changé depuis le serveur mpd
-         * si player -> c'est play/pause/stop, etc
-         * si mixer -> c'est le volume
-         * si options-> c'est repeat/random ou un autre
-         */
         $scope.$on('onUpdate', function (event, data) {
-            console.log(data.event);
-            if(data.event == 'player'){
-                $scope.$apply(function () {
-                    $scope.player.status = data.mpd.status;
-                });
-
-            }else if(data.event == 'options'){
-                //on met à jout le status pour montrer si on a sélectionné random/repeat
-                console.log('on updated');
-                console.log(data.mpd);
-                $scope.$apply(function () {
-                    $scope.player.status = data.mpd.status;
-                });
-            }else if(data.event =='playlist'){
-                $scope.$apply(function () {
-                   $scope.player.playlist = data.mpd.playlist;
-                });
-            }
+            $scope.$apply(function () {
+               $scope.player = data.mpd;
+            });
         });
 
         $scope.$on('onDisconnect', function(event, data){
@@ -136,7 +105,6 @@ function main_ctrl($scope, $ionicPlatform, $timeout, $ionicModal, $ionicListDele
         };
 
         $scope.volPlus = function () {
-            console.log("plus");
             MPDService.volPlus();
         };
         $scope.volMinus = function () {
